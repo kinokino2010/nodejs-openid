b97c190934238c3cbd4fd7db8a8c13b391ed2ab7
KINOKINO <kinokino2010@gmail.com>
2014-09-07 08:06:59 +0900

    delete disused file

--- routes/timeline.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/timeline', function(req, res) {
-  res.render('timeline', { title: 'Time Line' });
-});
-
-module.exports = router;
